b24b34522e37319767f536b179a93190b0060f7d
gamartins013 <gamartins013@gmail.com>
2023-03-10 01:32:01 -0300

    feat: adicionando configuracao de deploy

---
+++ src/main/java/site/infinityflow/InfinityflowApplication.java
@@ -0,0 +1,13 @@
+package site.infinityflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityflowApplication.class, args);
+	}
+
+}
