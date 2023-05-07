8924ff9035f025eb26f99175193d13a53df89d8a
Ricardo Gava <ric.gva@gmail.com>
2023-05-07 16:18:57 -0300

    Alterando endpoint para receber várias presenças

--- src/main/java/site/infinityflow/usecases/registrapresenca/RegistraPresencaImpl.java
+++ src/main/java/site/infinityflow/usecases/registrapresenca/RegistraPresencaImpl.java
@@ -1,5 +1,6 @@
 package site.infinityflow.usecases.registrapresenca;
 
+import jakarta.persistence.Transient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import site.infinityflow.adapters.mysql.repository.AlunosRepository;
@@ -8,6 +9,9 @@ import site.infinityflow.adapters.mysql.repository.PresencasRepository;
 import site.infinityflow.entities.sistema.Aluno;
 import site.infinityflow.entities.sistema.Aula;
 import site.infinityflow.entities.sistema.Presenca;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -19,13 +23,17 @@ public class RegistraPresencaImpl implements RegistraPresenca {
     private final PresencasRepository presencasRepository;
 
     @Override
-    public void execute(RegistraPresencaRequest request) {
-        Aluno aluno = alunosRepository.getReferenceById(request.getIdAluno());
-        Aula aula = aulasRepository.getReferenceById(request.getIdAula());
-        Presenca presenca = new Presenca();
-        presenca.setAluno(aluno);
-        presenca.setAula(aula);
-        presenca.setSituacao(request.getSituacao().getCodigo());
-        presencasRepository.save(presenca);
+    @Transient
+    public void execute(RegistraPresencaRequest registraPresencasRequest) {
+        Aula aula = aulasRepository.getReferenceById(registraPresencasRequest.getIdAula());
+        List<Presenca> presencas = new ArrayList<>();
+        for (var presencaRequest : registraPresencasRequest.getPresencasRequests()) {
+            Presenca presenca = new Presenca();
+            presenca.setAluno(alunosRepository.getReferenceById(presencaRequest.getIdAluno()));
+            presenca.setAula(aula);
+            presenca.setSituacao(presencaRequest.getSituacao().getCodigo());
+            presencas.add(presenca);
+        }
+        presencasRepository.saveAll(presencas);
     }
 }
