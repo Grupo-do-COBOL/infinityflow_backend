b5a5dbcb55a8813143230bc8e24700f6668b490a
Ricardo Gava <ric.gva@gmail.com>
2023-05-07 23:19:33 -0300

    Adição do Filtro Request para o relatório

--- src/main/java/site/infinityflow/usecases/gerarelatorio/RelatorioFiltroRequest.java
+++ src/main/java/site/infinityflow/usecases/gerarelatorio/RelatorioFiltroRequest.java
@@ -1,4 +1,15 @@
 package site.infinityflow.usecases.gerarelatorio;
 
-public class RelatorioRequest {
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class RelatorioFiltroRequest {
+    private String data;
+    private String anoLetivo;
+    private String turma;
+    private String professor;
+    private String disciplina;
+    private String aluno;
 }
