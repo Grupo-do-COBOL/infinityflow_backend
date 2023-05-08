3be682bbe47c244816709a21530dc0542fb9081d
Ricardo Gava <ric.gva@gmail.com>
2023-05-07 23:39:11 -0300

    Correção serviceÃ

--- src/main/java/site/infinityflow/usecases/gerarelatorio/GeraRelatorioImpl.java
+++ src/main/java/site/infinityflow/usecases/gerarelatorio/GeraRelatorioImpl.java
@@ -1,5 +1,8 @@
 package site.infinityflow.usecases.gerarelatorio;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GeraRelatorioImpl implements GeraRelatorio {
     @Override
     public RelatorioResponse execute(RelatorioFiltroRequest relatorioFiltroRequest) {
